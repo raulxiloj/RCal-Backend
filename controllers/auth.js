6f4655677d567a6fe0b70b6b33396116f6695cb5
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-07-21 10:08:02 -0600

    User model created and user created in the db (Checking if theres not a user with the same email and encrypting the password)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,44 @@
 const { response } = require('express'); //Just to have the intellisense
-const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
 
-const createUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
 
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register'
-    })
+    try {
+
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'A user is registered with that email'
+            })
+        }
+
+        user = new User(req.body);
+
+        //Encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
+        await user.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact the admin'
+        })
+    }
 
 }
 
