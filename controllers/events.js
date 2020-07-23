68de8fd9c842e08572cbd9e82724bc9d3a2365e7
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-07-22 18:48:07 -0600

    [add] Necessary middlewares applied to event routes and controllers finished

--- controllers/events.js
+++ controllers/events.js
@@ -1,46 +1,126 @@
 const { response } = require("express");
+const Event = require("../models/Event");
 
-const createEvent = (req, res = response) => {
+const createEvent = async (req, res = response) => {
 
-    res.status(201).json({
-        ok: true,
-        msg: 'Event created'
-    });
+    const event = new Event(req.body);
+
+    try {
+
+        event.user = req.uid;
+        const eventSaved = await event.save();
+
+        res.status(201).json({
+            ok: true,
+            event: eventSaved
+        });
+
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact the admin'
+        })
+    }
 
 }
 
-const getEvents = (req, res = response) => {
+const getEvents = async (req, res = response) => {
+
+    const events = await Event.find().populate('user', 'name');
 
     res.status(200).json({
         ok: true,
-        msg: 'Events retrieved'
+        events
     });
 
 }
 
-const updateEvent = (req, res = response) => {
+const updateEvent = async (req, res = response) => {
 
     const { id } = req.params;
+    const uid = req.uid;
 
-    console.log(id);
+    try {
 
-    res.status(200).json({
-        ok: true,
-        msg: 'Event updated'
-    });
+        const event = await Event.findById(id);
+
+        if (!event) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Event not found'
+            });
+        }
+
+        if (event.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Unauthorized to edit this event'
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventUpdated = await Event.findByIdAndUpdate(id, newEvent, { new: true });
+
+        res.status(200).json({
+            ok: true,
+            eventUpdated
+        });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact the admin'
+        })
+    }
+
+
 
 }
 
-const deleteEvent = (req, res = response) => {
+const deleteEvent = async (req, res = response) => {
 
     const { id } = req.params;
+    const uid = req.uid;
 
-    console.log(id);
+    try {
 
-    res.status(200).json({
-        ok: true,
-        msg: 'Event deleted'
-    });
+        const event = await Event.findById(id);
+
+        if (!event) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Event not found'
+            });
+        }
+
+        if (event.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Unauthorized to delete this event'
+            });
+        }
+
+        await Event.findByIdAndDelete(id);
+
+        res.status(200).json({
+            ok: true,
+            msg: 'Event deleted'
+        });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact the admin'
+        })
+    }
 
 }
 
--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,42 @@
+/*
+    Event routes: /api/events
+*/
+
 const { Router } = require("express");
 const checkJWT = require("../middlewares/check-jwt");
 const { createEvent, getEvents, updateEvent, deleteEvent } = require("../controllers/events");
+const { check } = require("express-validator");
+const fieldValidator = require("../middlewares/field-validator");
+const isDate = require("../helpers/isDate");
 const router = Router();
 
+//All routes need to be validated by the JWT
 router.use(checkJWT);
 
-router.post('/create', createEvent);
+//Create an event
+router.post('/create',
+    [
+        check('title', 'The title is required').notEmpty(),
+        check('start', 'Starting date is required').custom(isDate),
+        check('end', 'Ending date is required').custom(isDate),
+        fieldValidator
+    ],
+    createEvent);
+
+//Get all the events    
 router.get('/', getEvents);
-router.put('/update/:id', updateEvent);
+
+//Update an event
+router.put('/update/:id',
+    [
+        check('title', 'The title is required').notEmpty(),
+        check('start', 'Starting date is required').custom(isDate),
+        check('end', 'Ending date is required').custom(isDate),
+        fieldValidator
+    ]
+    , updateEvent);
+
+//Delete an event
 router.delete('/delete/:id', deleteEvent);
 
 module.exports = router;
