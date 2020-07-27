ea7680746da2ab1381a78a67e2ab2417fa368531
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-07-26 22:23:27 -0600

    [release] React app added to the public folder and first release with backend and frontend

---
+++ public/precache-manifest.59ff54873be69f5a8a0068b01168a9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c42bc91dc11e1f973fa9dd140134bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f3d7ebe56b434880ed",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "d8a99cff55664ca83424",
+    "url": "/static/css/main.fb681e12.chunk.css"
+  },
+  {
+    "revision": "a9f3d7ebe56b434880ed",
+    "url": "/static/js/2.fc6eb01f.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.fc6eb01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a99cff55664ca83424",
+    "url": "/static/js/main.df819ae4.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
